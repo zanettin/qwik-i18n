2836f766302c78079f4793f5f6a331858188f210
Roman Zanettin <roman.zanettin@gmail.com>
2023-01-19 22:40:06 +0100

    latest pkgs

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,14 +1,10 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { RequestHandler } from "@builder.io/qwik-city";
 import Header from "../components/header/header";
 import { extractLang } from "../i18n";
 
-export const onRequest: RequestHandler = ({ request, response }) => {
-  response.locale = extractLang(
-    request.headers.get("accept-language"),
-    request.url
-  );
-  return {};
+export const onRequest = ({ request, locale }) => {
+  const lang = extractLang(request.headers.get("accept-language"), request.url);
+  locale(lang);
 };
 
 export default component$(() => {
